0380baabb955ab2a6b6bd364a51c8ccb4c980dd7
Jaime Vega <jhuesos@gmail.com>
2012-10-04 16:52:55 +0200

    Finished first version of Promise toolkit with basic set of unit tests (JSDoc needs to be completed)

--- Promise.js
+++ Promise.js
@@ -1,5 +1,4 @@
 /*jslint indent: 4, browser: true */
-/*globals */
 
 /**
  * @author Jaime Vega (jhuesos@gmail.com)
@@ -8,5 +7,54 @@
 (function () {
 	"use strict";
 
+	var Promise = function () {
+		if (!this instanceof Promise) {
+			return new Promise(arguments);
+		}
 
+		this._queue = [];
+
+		return this;
+	};
+
+    /**
+     * Chain operations depending bases upon state change in of our promises. Both callbacks expects to be called with
+     * one parameter, the value obtained after the operation was finished.
+     * @param {Function} [onResolved] Callback to call if the operation was resolved.
+     * @param {Function} [onRejected] Callback to call if the operation was rejected.
+     * @return {Promise} Return the object itself to cascade calls.
+     */
+	Promise.prototype.then = function (onResolved, onRejected) {
+		var callbacks = {
+			resolvedCB : onResolved,
+			rejectedCB : onRejected
+		};
+
+		this._queue.push(callbacks);
+
+		return this;
+	};
+
+	Promise.prototype.resolve = function (arg) {
+		var cbs = this._queue.shift();
+
+		if (cbs && typeof cbs.resolvedCB === 'function') {
+			cbs.resolvedCB(arg);
+		}
+
+		return this;
+	};
+
+	Promise.prototype.reject = function (arg) {
+		var cbs = this._queue.shift();
+
+		if (cbs && typeof cbs.rejectedCB === 'function') {
+			cbs.rejectedCB(arg);
+		}
+
+		return this;
+	};
+
+    // Export Promise to global scope
+	window.Promise = Promise;
 }());
--- PromiseTest.js
+++ PromiseTest.js
@@ -1,5 +1,5 @@
-/*jslint indent:4, browser:true, newcap:true */
-/*global TestCase, assertTrue, assertFalse, assertObject, assertFunction, assertEquals, assertException */
+/*jshint indent:4, browser:true, newcap:true */
+/*global Promise, AsyncTestCase, TestCase, assertTrue, assertFalse, assertFunction */
 
 /**
  * @author Jaime Vega (jhuesos@gmail.com)
@@ -8,9 +8,109 @@
 (function () {
 	"use strict";
 
-	TestCase("Promise", {
-		'test dummy test' : function () {
+	// Preparation code
 
-		}
-	});
+	// AsyncFn is a method who simulates the behaviour of an async function called using setTimeout and uses Promise.
+	var asyncFn = function (successful) {
+		var fnToCall,
+			promise = new Promise(),
+			success = function () {
+				promise.resolve();
+			},
+			fail = function () {
+				promise.reject();
+			};
+
+
+		fnToCall = successful ? success : fail;
+
+		setTimeout(function () {
+			fnToCall();
+		}, 10);
+
+		return promise;
+	};
+
+	// General Basic promise test suite
+	var PromiseTestSuite = new TestCase("Promise");
+
+	PromiseTestSuite.prototype.testContainsThenMethod = function () {
+		var promise = new Promise();
+
+		assertFunction(promise.then);
+	};
+
+	PromiseTestSuite.prototype.testContainsResolveMethod = function () {
+		var promise = new Promise();
+
+		assertFunction(promise.resolve);
+	};
+
+	PromiseTestSuite.prototype.testContainsRejectMethod = function () {
+		var promise = new Promise();
+
+		assertFunction(promise.reject);
+	};
+
+	// Then / Promise full implementation test suite
+	var ThenTestSuite = new AsyncTestCase("Then"),
+		promise;
+
+	ThenTestSuite.prototype.setUp = function () {
+		promise = new Promise();
+	};
+
+	ThenTestSuite.prototype.testReturnsPromiseInstanceForCascading = function () {
+		var fn = function () {};
+
+		assertTrue(promise.then(fn) instanceof Promise);
+	};
+
+	ThenTestSuite.prototype.testSimpleThenWithSuccessCB = function (queue) {
+
+		var called = false;
+
+		queue.call("Step 1: Run async method with promises", function (callbacks) {
+
+			var fn = callbacks.add(function () {
+				called = true;
+			});
+
+			asyncFn(true).then(fn);
+
+			// At this point, since asyncFn is an async fn, called should be still false
+			assertFalse(called);
+		});
+
+		queue.call("Step 2: Check if resolve cb was called", function () {
+			assertTrue(called);
+		});
+	};
+
+	ThenTestSuite.prototype.testRejectedRequestWithBothCBs = function (queue) {
+		var successCalled = false,
+			rejectedCalled = false;
+
+		queue.call("Step 1: Run async method with promises and rejected request", function (callbacks) {
+
+			var successFn = function () {
+					successCalled = true;
+				},
+				rejectFn = callbacks.add(function () {
+					rejectedCalled = true;
+				});
+
+			// Calling async fn with false, we make sure the "request" fails
+			asyncFn(false).then(successFn, rejectFn);
+
+			// At this point, since asyncFn is an async fn, called should be still false
+			assertFalse(rejectedCalled);
+			assertFalse(successCalled);
+		});
+
+		queue.call("Step 2: Check if only rejected cb was called", function () {
+			assertTrue(rejectedCalled);
+			assertFalse(successCalled);
+		});
+	};
 }());
